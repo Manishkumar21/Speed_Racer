1c2a2e6242332a7bb4fd8326b8846195d687eea8
Manish <khiladimanu1@gmail.com>
2017-07-07 12:38:29 +0530

    Commiting the HTML demo Project

---
+++ index_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'vlightbox1',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:false,overlayOpacity:0,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
